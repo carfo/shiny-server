e95b6b03c8d71923bc0d5bcdb19a99d90c487458
Fabio <carfo@users.noreply.github.com>
2019-01-05 16:57:32 +0100

    Update global.R

--- Dome/global.R
+++ Dome/global.R
@@ -1,8 +1,22 @@
 # load queries and helpers
 source("functions.R")
 # load libraries
-load_lib(c("shiny","DT","data.table","DBI","RSQLite","digest","lubridate","config",
-           "shinyWidgets","shinythemes","dplyr","pool","stringr","stringi"))
+# load_lib(c("shiny","DT","data.table","DBI","RSQLite","digest","lubridate","config",
+#           "shinyWidgets","shinythemes","dplyr","pool","stringr","stringi"))
+library(shiny)
+library(DT)
+library(data.table)
+library(DBI)
+library(RSQLite)
+library(digest)
+library(lubridate)
+library(config)
+library(shinyWidgets)
+library(shinythemes)
+library(dplyr)
+library(pool)
+library(stringr)
+library(stringi)
 # load config
 #config<-config::get(file = "Sicurezza/config.yml")
 #credentials_file<-"www/users.dat"
