585d079324b7d8c3191944c4b4646a1c2b9b8982
Fabio <carfo@users.noreply.github.com>
2019-02-28 18:39:43 +0100

    img update

--- DragNDrop/app.R
+++ DragNDrop/app.R
@@ -10,18 +10,23 @@ ui <- fluidPage(
     # Sidebar with a slider input
     sidebarPanel(
       tags$style(type = "text/css",".dragelement {font-size: large};"),
-      br(), br(),
+      h2("Fattori individuati dal Gruppo 1"),
       dragSetUI("div1", textval = as.list(items[V2==group])$V1),
       width = 3
     ),
     mainPanel(tags$style(type = "text/css",".dropelement {background-image: url('background.png');
-                         height:600px; width:850px"),
-              jqui_resizable(dropUI("div2", row_n = 20, col_n = 10),
+                         height:600px; width:850px}"),
+              tags$style(type = "text/css",".footer {background-color: #D2232A}"),
+              jqui_resizable(dropUI("div2", row_n = 40, col_n = 20),
                              options = list(minWidth= 850, minHeight = 600, maxWidth= 850, maxHeight = 600))
               #verbatimTextOutput("foo"),
     )
-  )
+  ),
+  div(class = "footer",
+      br(),
+      tags$img(src="logo.png"))
 )
+
 server = shinyServer(function(input, output,session) {
   session$onSessionEnded(function(){
     #rm(config,pos=.GlobalEnv)
